7ac2660432e997e298fb3531c152985aa2cb5e5c
Brendan Barnes <barne856@umn.edu>
2022-09-27 23:27:03 +0000

    CLI tests on local filesystem

--- tests/test_ras_remodeler.py
+++ tests/test_ras_remodeler.py
@@ -1,9 +1,32 @@
+"""Integration tests for ras remodeler"""
+from os.path import exists
+from click.testing import CliRunner
 from ras_remodeler import create_plan_tmp_hdf, set_plan_hdf_hydrograph
+import h5py
+import os
+from fs_util import get_file
+from hdf_util import copy_hdf
 
 
 def test_create_plan_tmp_hdf():
-    pass
+    """Test create tmp HDF file with Results group removed"""
+    src_file = "tests/data/Muncie.p04.hdf"
+    runner = CliRunner()
+    runner.invoke(create_plan_tmp_hdf, [src_file])
+    assert exists("tests/data/Muncie.p04.tmp.hdf")
 
 
 def test_set_plan_hdf_hydrograph():
-    pass
+    """Test update hydrograph of HDF file"""
+    src_file = "tests/data/Muncie.p04.hdf"
+    temp_file = get_file(ext=".hdf")
+    copy_hdf(src_file, temp_file, ["Results"])
+    hydrograph_name = "River: White  Reach: Muncie  RS: 15696.24"
+    hydrograph = "tests/data/hydrograph.dss:/REGULAR/TIMESERIES/FLOW//1HOUR/Ex1/"
+    runner = CliRunner()
+    runner.invoke(set_plan_hdf_hydrograph, [
+                  temp_file, hydrograph_name, hydrograph])
+    with h5py.File(temp_file) as file:
+        dataset = file["/Event Conditions/Unsteady/Boundary Conditions/Flow Hydrographs/River: White  Reach: Muncie  RS: 15696.24"]
+        assert dataset.shape == (7, 2)
+    os.remove(temp_file)
